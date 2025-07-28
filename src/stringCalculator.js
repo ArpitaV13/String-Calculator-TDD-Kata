e430500a010a14d9d102e8484f9a87fec3710ade
ArpitaV13 <arpitaverma8885@gmail.com>
2025-07-28 22:26:50 +0530

    handled custom delimeter

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,7 +1,13 @@
 function add(numbers){
     if(!numbers) return 0;
-    const parts = numbers.split(/[\n,]/).map(Number);
-    return parts.reduce((sum, num)=>sum+num, 0);
+
+    let delimeter= /[\n,]/;
+    if(numbers.startsWith('//')){
+        const parts = numbers.split('\n');
+        delimeter = new RegExp(parts[0].slice(2));
+        numbers= parts[1];
+    }
+    return numbers.split(delimeter).map(Number).reduce((sum,num)=>(sum+num),0);
 }
 
 module.exports = {add};
