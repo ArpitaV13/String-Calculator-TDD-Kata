0d31bb7020da6aeec0cd2be41d5511fe4c66e6b4
ArpitaV13 <arpitaverma8885@gmail.com>
2025-07-28 22:13:25 +0530

    handled numbers input

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -3,3 +3,8 @@ const {add} = require('../src/stringCalculator')
 test('returns 0 for empty string', ()=>{
     expect(add("")).toBe(0);
 });
+
+test('returns number if single number string passed', () => {
+    expect(add("5")).toBe(5);
+  });
+  
